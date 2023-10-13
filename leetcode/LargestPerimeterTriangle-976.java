a6f84a78da638de14bb4096002fa5195bdb09d4b
M R Vimal <78142299+Vimalmr@users.noreply.github.com>
2023-10-13 10:05:55 +0530

    optimised the code

--- leetcode/LargestPerimeterTriangle-976.java
+++ leetcode/LargestPerimeterTriangle-976.java
@@ -5,21 +5,17 @@ class Solution {
         // Ex: Side A < Side B + Side C
 
         Arrays.sort(nums); //Sort the Array
-        int flag =0,n=nums.length,res=0;
+        int n=nums.length,res=0;
 
         for(int i=n-1;i>1;i--){ 
             //Checking whether the Largest side is smaller than addition other two side
             if(nums[i-1]+nums[i-2]>nums[i]){
                 res+=nums[i]+nums[i-1]+nums[i-2];
-                flag=1;
                 break;
             }
         }
 
         //Returning the answers
-        if(flag == 0){
-            return 0;
-        }
         return res;
     }
 }
