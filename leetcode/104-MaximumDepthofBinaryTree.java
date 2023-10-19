18f12146ebab39ec27f060137ec17bcd53b77da5
M R Vimal <78142299+Vimalmr@users.noreply.github.com>
2023-10-19 14:38:36 +0530

    Create 104-MaximumDepthofBinaryTree.java

---
+++ leetcode/104-MaximumDepthofBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null){
+            return 0;
+        }
+        q.add(root);
+        int count=0;
+        while(!q.isEmpty()){
+            int lvl = q.size();
+            for(int i=0;i<lvl;i++){
+                if(q.peek().left!=null){
+                    q.add(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.add(q.peek().right);
+                }
+                int fin = q.remove().val;
+            }
+            count++;
+        }
+        return count;
+    }
+}
