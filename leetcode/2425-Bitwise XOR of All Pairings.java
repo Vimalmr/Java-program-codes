41881816a4a25cdc0ed19dc244a21ce01b4ced03
M R Vimal <78142299+Vimalmr@users.noreply.github.com>
2023-12-28 13:55:07 +0530

    Update 2425-Bitwise XOR of All Pairings.java

--- leetcode/2425-Bitwise XOR of All Pairings.java
+++ leetcode/2425-Bitwise XOR of All Pairings.java
@@ -1,19 +1,17 @@
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        int v1=0,v2=0;
+        int v1=0;
         int n1 = nums2.length % 2, n2 = nums1.length % 2;
-        for(int i: nums1){
-            v1 = v1 ^ i;
-            if(n1==0){
+        if(n1!=0){
+            for(int i: nums1){
                 v1 = v1 ^ i;
             }
         }
-        for(int i: nums2){
-            v2 = v2 ^ i;
-            if(n2==0){
-                v2 = v2 ^ i;
+        if(n2!=0){
+            for(int i: nums2){
+                v1 = v1 ^ i;
             }
         }
-        return v1^v2;
+        return v1;
     }
 }
